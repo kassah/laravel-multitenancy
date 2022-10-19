fbf7d376c8ecd37927fb99d0d3082fbec3c113d1
William Lightning <kassah@gmail.com>
2022-10-19 23:28:14 +0000

    Switch tenants during factory model children creation.

--- database/factories/TenantFactory.php
+++ database/factories/TenantFactory.php
@@ -3,6 +3,7 @@
 namespace Spatie\Multitenancy\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use Spatie\Multitenancy\Models\Tenant;
 use Spatie\Multitenancy\Tests\TestClasses\User;
@@ -18,5 +19,16 @@ class TenantFactory extends Factory
             'domain' => $this->faker->unique()->domainName,
             'database' => $this->faker->userName,
         ];
+    }
+
+    protected function createChildren(Model $model): void
+    {
+        if ($model instanceof Tenant) {
+            $model->execute(function() use ($model) {
+                parent::createChildren($model);
+            });
+        } else {
+            parent::createChildren($model);
+        }
     }
 }
